268dd358d0784f4ab468620950908655e24362ad
ikusteu <ivankust57@gmail.com>
2023-08-07 02:32:30 +0200

    Add right-click menu functionality to the extension

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -1,3 +1,52 @@
+interface Note {
+	content: string;
+	source: string;
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		id: "add-note",
+		title: "Add Note",
+		contexts: ["selection"]
+	});
+
+	chrome.contextMenus.create({
+		id: "abstract",
+		parentId: "add-note",
+		title: "Abstract",
+		contexts: ["selection"]
+	});
+	chrome.contextMenus.create({
+		id: "main",
+		parentId: "add-note",
+		title: "Main",
+		contexts: ["selection"]
+	});
+	chrome.contextMenus.create({
+		id: "summary",
+		parentId: "add-note",
+		title: "Summary",
+		contexts: ["selection"]
+	});
+});
+
+const sections: Record<string, Note[]> = {
+	abstract: [],
+	main: [],
+	summary: []
+};
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.parentMenuItemId === "add-note") {
+		sections[info.menuItemId].push({
+			content: info.selectionText || "",
+			source: tab?.title || ""
+		});
+	}
+
+	console.log(sections);
+});
+
 chrome.runtime.onMessage.addListener((e: any) => {
 	console.log(e);
 	return true;
