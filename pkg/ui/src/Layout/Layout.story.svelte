c5ee4b58fb76e9743143c22da0266a62a10f331d
ikusteu <ivankust57@gmail.com>
2023-08-29 08:50:13 +0200

    Crate a Layout component

---
+++ pkg/ui/src/Layout/Layout.story.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+
+	import Layout from "./Layout.svelte";
+
+	export let Hst: Hst;
+
+	const resources = [
+		"High frequency trading",
+		"Does Algorythmic Trading Improve Liquidity",
+		"Foucault (2023)",
+		"High Frequency Trading and Price Discovery"
+	].map((name) => ({
+		name,
+		href: "",
+		current: false
+	}));
+
+	const projects = ["Discertation", "Case Study: 1"].map((name) => ({
+		name,
+		href: "",
+		current: false
+	}));
+</script>
+
+<Hst.Story title="Layout">
+	<Layout sidebarOpen {projects} {resources} />
+</Hst.Story>
---
+++ pkg/ui/src/Layout/Layout.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { Menu } from "lucide-svelte";
+	import { createEventDispatcher } from "svelte";
+
+	import { NewEntitySideNavButton, SidebarItem } from "../Sidebar";
+
+	export let sidebarOpen = false;
+	export let currentRoute = "";
+
+	interface NavItem {
+		name: string;
+		href: string;
+		current: boolean;
+	}
+
+	export let resources: NavItem[] = [];
+	export let projects: NavItem[] = [];
+
+	const dispatch = createEventDispatcher<{ createproject: "" }>();
+	const createProject = () => dispatch("createproject");
+
+	const toggleOpenSidebar = () => (sidebarOpen = !sidebarOpen);
+</script>
+
+<div class="flex h-screen flex-col">
+	<div class="basis-[5%]">
+		<header class="flex h-[4.5rem] w-full items-center justify-start border-b border-sky-900 bg-gray-900 px-4 py-4">
+			<button
+				class="flex h-10 w-10 items-center justify-center rounded-sm border border-gray-900 text-white {sidebarOpen && 'bg-gray-600'}"
+				on:click={toggleOpenSidebar}
+			>
+				<Menu />
+			</button>
+		</header>
+	</div>
+
+	<main class="flex basis-[95%] items-stretch divide-x divide-gray-300 overflow-y-auto">
+		{#if sidebarOpen}
+			<section
+				id="sidebar-section"
+				class="h-full shrink-0 basis-32 overflow-y-auto overflow-x-hidden whitespace-nowrap bg-gray-50 sm:basis-56"
+			>
+				<SidebarItem name="Notes" href="/notes" current={currentRoute.includes("notes")} />
+				<div class="p-3 text-sm font-bold">Resources</div>
+				{#each resources as resource}
+					<SidebarItem name={resource.name} href={resource.href} nested current={resource.current} />
+				{/each}
+				<div class="p-3 text-sm font-bold">Projects</div>
+				{#each projects as project}
+					<SidebarItem name={project.name} nested href={project.href} current={project.current} />
+				{/each}
+				<div class="pl-7">
+					<NewEntitySideNavButton label="New Project" on:click={createProject} />
+				</div>
+			</section>
+		{/if}
+
+		<section class="flex h-full w-full flex-col justify-start gap-y-8 overflow-auto py-8">
+			<slot />
+		</section>
+	</main>
+</div>
