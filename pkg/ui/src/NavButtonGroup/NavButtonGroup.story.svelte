0d19b4c2b64f469b063b155b0171ee8099e6cedb
ikusteu <ivankust57@gmail.com>
2023-09-02 16:25:41 +0200

    Create buttons

---
+++ pkg/ui/src/NavButtonGroup/NavButtonGroup.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+
+	import NavButtonGroup from "./NavButtonGroup.svelte";
+
+	export let Hst: Hst;
+
+	const options = ["Sections", "Subsections", "Text"];
+</script>
+
+<Hst.Story title="Nav Button Group">
+	<NavButtonGroup on:change={(e) => console.log(e.detail)} {options} />
+</Hst.Story>
